ff345dc2619ca886d84a3aff9e4d0cae11cc82fb
The Ghost <djblasterx@gmail.com>
2017-03-14 18:18:33 +0300

    First commit: PixelLauncher 4*4 and iconset

---
+++ vendor.mk
@@ -0,0 +1,8 @@
+# NexusLauncher, modified version and its iconset
+PRODUCT_COPY_FILES += \
+    vendor/lurepheonix/prebuilt/NexusLauncher/NexusLauncher.apk:system/app/NexusLauncher/NexusLauncher.apk \
+    vendor/lurepheonix/prebuilt/Pixel/Pixel.apk:system/app/Pixel/Pixel.apk
+    
+# 
+    
+ 
